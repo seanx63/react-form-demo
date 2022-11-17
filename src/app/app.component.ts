50d48836875c4ec765278f45ed47037d27dc070e
Sean Gates <seanx63@gmail.com>
2022-11-17 11:51:45 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'react-form-demo';
+  title = 'react-forms';
+  loginform = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    age: new FormControl('', Validators.required),
+    mobile: new FormControl('', Validators.required)
+  })
+  loginuser() {
+    console.log(this.loginform.value)
+  }
+  get name() {
+    return this.loginform.get('name');
+  }
+  get email() {
+    return this.loginform.get('email');
+  }
+  get age() {
+    return this.loginform.get('age');
+  }
+  get mobile() {
+    return this.loginform.get('mobile');
+  }
+  send() {
+    alert('Sent!')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input'
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
